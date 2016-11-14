46dfc44ab5cd29fba31bab2eaa4f840a3135248d
nicoskia <zeldawg@gmail.com>
2016-11-14 12:08:04 -0500

    JS Edits

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var imageArrayC = new Array();
+var imageArrayC = [];
     imageArrayC[0] = "images/CBSA/c-1.jpg";
     imageArrayC[1] = "images/CBSA/c-2.jpg";
     imageArrayC[2] = "images/CBSA/c-3.jpg";
@@ -7,35 +7,15 @@ var imageArrayC = new Array();
     imageArrayC[5] = "images/CBSA/c-6.jpg";
     imageArrayC[6] = "images/CBSA/c-7.jpg";
 
-
-function doC()
-    {
-    var rand = Math.floor(Math.random()*7);
-
-    var imgPath = "<img src='"+imageArrayC[rand]+"' alt='header' border='0' align='absmiddle' />";
-
+function doC() {
+	"use strict";
+    var rand = Math.floor(Math.random() * 7);
+    var imgPath = "<img src='"+imageArrayC[rand]+"' alt='C' border='0' align='absmiddle' />";
     document.getElementById("imageC").innerHTML = imgPath;
-
     }
 
-var imageArrayA = new Array();
-    imageArrayA[0] = "images/CBSA/a-1.JPG";
-    imageArrayA[1] = "images/CBSA/a-2.JPG";
-    imageArrayA[2] = "images/CBSA/a-3.JPG";
-    imageArrayA[3] = "images/CBSA/a-4.JPG";
-    imageArrayA[4] = "images/CBSA/a-5.JPG";
 
-function doA()
-    {
-    var rand = Math.floor(Math.random()*5);
-
-    var imgPath = "<img src='"+imageArrayA[rand]+"' alt='header' border='0' align='absmiddle' />";
-
-    document.getElementById("imageA").innerHTML = imgPath;
-
-    }
-
-var imageArrayB = new Array();
+var imageArrayB = [];
     imageArrayB[0] = "images/CBSA/b-1.jpg";
     imageArrayB[1] = "images/CBSA/b-2.jpg";
     imageArrayB[2] = "images/CBSA/b-3.jpg";
@@ -43,33 +23,41 @@ var imageArrayB = new Array();
     imageArrayB[4] = "images/CBSA/b-5.jpg";
     imageArrayB[5] = "images/CBSA/b-6.jpg";
 
-function doB()
-    {
+function doB(){
+	"use strict";
     var rand = Math.floor(Math.random()*6);
-
-    var imgPath = "<img src='"+imageArrayB[rand]+"' alt='header' border='0' align='absmiddle' />";
-
+    var imgPath = "<img src='"+imageArrayB[rand]+"' alt='B' border='0' align='absmiddle' />";
     document.getElementById("imageB").innerHTML = imgPath;
-
     }
 
 
-
-var imageArrayS = new Array();
+var imageArrayS = [];
     imageArrayS[0] = "images/CBSA/s-1.jpg";
     imageArrayS[1] = "images/CBSA/s-2.jpg";
     imageArrayS[2] = "images/CBSA/s-3.jpg";
     imageArrayS[3] = "images/CBSA/s-4.jpg";
     imageArrayS[4] = "images/CBSA/s-5.jpg";
 
-function doS()
-    {
+function doS(){
+	"use strict";
     var rand = Math.floor(Math.random()*5);
-
-    var imgPath = "<img src='"+imageArrayS[rand]+"' alt='header' border='0' align='absmiddle' />";
-
+    var imgPath = "<img src='"+imageArrayS[rand]+"' alt='S' border='0' align='absmiddle' />";
     document.getElementById("imageS").innerHTML = imgPath;
+    }
 
+
+var imageArrayA = [];
+    imageArrayA[0] = "images/CBSA/a-1.JPG";
+    imageArrayA[1] = "images/CBSA/a-2.JPG";
+    imageArrayA[2] = "images/CBSA/a-3.JPG";
+    imageArrayA[3] = "images/CBSA/a-4.JPG";
+    imageArrayA[4] = "images/CBSA/a-5.JPG";
+
+function doA(){
+	"use strict";
+    var rand = Math.floor(Math.random()*5);
+    var imgPath = "<img src='"+imageArrayA[rand]+"' alt='A' border='0' align='absmiddle' />";
+    document.getElementById("imageA").innerHTML = imgPath;
     }
 
 doC();
